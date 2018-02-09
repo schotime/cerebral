b20f9b62350155432612be868d2f6f20c85f0c2c
Adam Schroder <aschroder@inlogik.com>
2018-02-10 00:28:15 +1100

    WIP: strongly typed route def

--- packages/demos/todomvc-fluent/src/module/index.ts
+++ packages/demos/todomvc-fluent/src/module/index.ts
@@ -2,17 +2,15 @@ import { Module, Computed, Dictionary } from '@cerebral/fluent';
 import Router from '@cerebral/router';
 import * as signals from './sequences';
 import * as providers from './providers';
-import { State, Todo, Signals } from './types';
+import { State, Todo } from './types';
 import { counts, isAllChecked, visibleTodosUids } from './computed';
-
-const nameof = <T>(name: keyof T) => name;
 
 const router = Router({
   onlyHash: true,
   query: true,
   routes: [
-    { path: '/', signal: nameof<Signals>('redirectToAll') },
-    { path: '/:filter', signal: nameof<Signals>('changeFilter') }
+    { path: '/', signal: signals.redirectToAll },
+    { path: '/:filter', signal: signals.changeFilter }
   ]
 });
 
